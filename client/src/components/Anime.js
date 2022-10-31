86fa2c9256d0ddef8cb1880a87e0e6e574f3945e
Ishan <ishanwijes@gmail.com>
2022-10-31 17:45:44 +1100

    added back to search results button on each category page

--- client/src/components/Anime.js
+++ client/src/components/Anime.js
@@ -164,17 +164,34 @@ export const Anime = () => {
       {
       singlePoster ? (
 
-        <div className="flex flex-row items-center justify-center mt-32">
-          <img
-          src={posterElementObject.src}
-          alt="single poster image"
-          className="w-[300px] mr-8 border-2 border-black shadow-lg shadow-gray-800"
-          />
+        <div>
 
-          <div className="bg-gray-600 py-8 px-8 border-2 border-black rounded-lg text-gray-300 ml-8 max-w-[400px]">
-            <p className="text-2xl mb-6">$ {posterElementObject.price}</p>
-            
-            <button className="bg-green-600 px-6 py-3 mx-auto rounded-lg hover:scale-105 duration-200 border-2 border-black text-xl w-full text-center mb-6 text-black">Add to Cart</button>
+          <button
+          className="bg-green-600 py-3 rounded-lg hover:scale-105 duration-200 border-2 border-black text-xl w-full mt-12 text-black w-[250px] ml-12"
+          onClick={() => {
+            setSinglePoster(false)
+            setPosterElementObject({
+              price: '',
+              src: ''
+            })
+          }}
+          >
+            Back to Search Results
+          </button>
+
+          <div className="flex flex-row items-center justify-center mt-32">
+            <img
+            src={posterElementObject.src}
+            alt="single poster image"
+            className="w-[300px] mr-8 border-2 border-black shadow-lg shadow-gray-800"
+            />
+
+            <div className="bg-gray-600 py-8 px-8 border-2 border-black rounded-lg text-gray-300 ml-8 max-w-[400px]">
+              <p className="text-2xl mb-6">$ {posterElementObject.price}</p>
+              
+              <button className="bg-green-600 px-6 py-3 mx-auto rounded-lg hover:scale-105 duration-200 border-2 border-black text-xl w-full text-center mb-6 text-black">Add to Cart</button>
+            </div>
+
           </div>
         </div>
 

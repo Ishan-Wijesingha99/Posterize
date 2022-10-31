ae1274b14d88bbeaeb08a37a0fd8e0d3d0782488
Ishan <ishanwijes@gmail.com>
2022-10-31 15:30:40 +1100

    updated cart and saved items page

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import singlePoster from '../images/posters/artAndNature/artandnature1.jpg'
 
 import { authenticateToken } from '../utils/AuthenticateToken'
+
 
 
 export const Cart = ({loggedIn, setLoggedIn}) => {
@@ -17,48 +17,13 @@ export const Cart = ({loggedIn, setLoggedIn}) => {
   }, [])
 
 
+
   return (
     <div className="min-h-screen my-24">
+
       <div className="flex flex-col items-center justify-center py-8 bg-gray-600 w-3/4 mx-auto border-2 border-black rounded-lg">
 
         <p className="text-3xl my-4 text-gray-300 font-silkscreen w-3/4 text-center">Items in Cart: </p>
-
-        <div className="flex flex-row items-center justify-between w-5/6 p-8 bg-gray-300 border-2 border-black rounded-lg mt-8">
-          <img
-          src={singlePoster}
-          alt="cart poster icon" 
-          className="h-[250px] border-2 border-black rounded-lg"
-          />
-
-          <div>
-            <p className="text-2xl mb-6">Price: A$39.99</p>
-
-            <label htmlFor="quantity" className="text-2xl">Quantity</label>
-            <br />
-            <input type="number" id="quantity" name="quantity" min="1" placeholder="Quantity" className="p-2 border-2 border-black rounded-lg text-black mt-2 mb-6 w-full"/>
-
-          </div>
-        </div>
-
-
-
-        <div className="flex flex-row items-center justify-between w-5/6 p-8 bg-gray-300 border-2 border-black rounded-lg mt-8">
-          <img
-          src={singlePoster}
-          alt="cart poster icon" 
-          className="h-[250px] border-2 border-black rounded-lg"
-          />
-
-          <div>
-
-            <p className="text-2xl mb-6">Price: A$39.99</p>
-
-            <label htmlFor="quantity" className="text-2xl">Quantity</label>
-            <br />
-            <input type="number" id="quantity" name="quantity" min="1" placeholder="Quantity" className="p-2 border-2 border-black rounded-lg text-black mt-2 mb-6 w-full"/>
-
-          </div>
-        </div>
 
       </div>
 
@@ -71,8 +36,6 @@ export const Cart = ({loggedIn, setLoggedIn}) => {
         <p className="text-xl my-4 text-gray-300 ml-8">Total: </p>
         <button className="text-xl my-4 ml-8 bg-green-600 px-6 py-3 rounded-lg hover:scale-105 duration-200 border-2 border-black text-black">Proceed to Checkout</button>
       </div>
-
-
       
     </div>
   )
--- client/src/components/SavedPage.js
+++ client/src/components/SavedPage.js
@@ -1,5 +1,4 @@
 import React, {useEffect} from "react";
-import singlePoster from '../images/posters/artAndNature/artandnature1.jpg'
 
 import { authenticateToken } from '../utils/AuthenticateToken'
 
@@ -21,46 +20,9 @@ export const SavedPage = ({loggedIn, setLoggedIn}) => {
 
   return (
     <div className="min-h-screen my-24">
+
       <div className="flex flex-col items-center justify-center py-8 bg-gray-600 w-3/4 mx-auto border-2 border-black rounded-lg">
-
         <p className="text-3xl my-4 text-gray-300 font-silkscreen w-3/4 text-center">Saved Items: 2</p>
-
-        <div className="flex flex-row items-center justify-between w-5/6 p-8 bg-gray-300 border-2 border-black rounded-lg mt-8">
-          <img
-          src={singlePoster}
-          alt="cart poster icon" 
-          className="h-[250px] border-2 border-black rounded-lg"
-          />
-
-          <div>
-            <p className="font-silkscreen text-xl mb-6">Poster Name</p>
-
-            <p className="text-2xl mb-6">Price: A$39.99</p>
-
-            <button className="bg-green-600 px-6 py-3 mx-auto flex items-center rounded-lg hover:scale-105 duration-200 border-2 border-black text-xl w-full">Add to Cart</button>
-
-          </div>
-        </div>
-
-
-
-        <div className="flex flex-row items-center justify-between w-5/6 p-8 bg-gray-300 border-2 border-black rounded-lg mt-8">
-          <img
-          src={singlePoster}
-          alt="cart poster icon" 
-          className="h-[250px] border-2 border-black rounded-lg"
-          />
-
-          <div>
-            <p className="font-silkscreen text-xl mb-6">Poster Name</p>
-
-            <p className="text-2xl mb-6">Price: A$39.99</p>
-
-            <button className="bg-green-600 px-6 py-3 mx-auto flex items-center rounded-lg hover:scale-105 duration-200 border-2 border-black text-xl w-full">Add to Cart</button>
-
-          </div>
-        </div>
-
       </div>
 
     </div>

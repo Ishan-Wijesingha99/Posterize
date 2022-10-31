28a06b6184400a51a787f2fba71913c329b34464
Ishan <ishanwijes@gmail.com>
2022-10-31 15:27:43 +1100

    animals page single poster view completed

--- client/src/components/Animals.js
+++ client/src/components/Animals.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 import animals1 from '../images/posters/animals/animals1.jpg'
 import animals2 from '../images/posters/animals/animals2.jpg'
@@ -24,40 +24,173 @@ import animals21 from '../images/posters/animals/animals21.jpg'
 
 
 
-const arrayOfImages = [animals1, animals2, animals3, animals4, animals5, animals6, animals7, animals8, animals9, animals10, animals11, animals12, animals13, animals14, animals15, animals16, animals17, animals18, animals19, animals20, animals21]
-
-const arrayOfJSX = arrayOfImages.map((imageString, i) => {
-  return (
-    <div className="flex flex-col items-center justify-center" key={i+1}>
-      <img
-      src={imageString}
-      alt={`Animals poster - ${i + 1}`}
-      className="border-black border-2 shadow-lg shadow-gray-800 transition ease-out hover:scale-105 duration-200 cursor-pointer"
-      />
-    </div>
-  )
-})
+const arrayOfImages = [
+  {
+    imgString: animals1,
+    price: '34.99'
+  }, 
+  {
+    imgString: animals2,
+    price: '34.99'
+  }, 
+  {
+    imgString: animals3,
+    price: '39.99'
+  }, 
+  {
+    imgString: animals4,
+    price: '34.99'
+  }, 
+  {
+    imgString: animals5,
+    price: '39.99'
+  }, 
+  {
+    imgString: animals6,
+    price: '34.99'
+  }, 
+  {
+    imgString: animals7,
+    price: '34.99'
+  }, 
+  {
+    imgString: animals8,
+    price: '34.99'
+  }, 
+  {
+    imgString: animals9,
+    price: '34.99'
+  }, 
+  {
+    imgString: animals10,
+    price: '39.99'
+  }, 
+  {
+    imgString: animals11,
+    price: '34.99'
+  }, 
+  {
+    imgString: animals12,
+    price: '34.99'
+  }, 
+  {
+    imgString: animals13,
+    price: '34.99'
+  }, 
+  {
+    imgString: animals14,
+    price: '39.99'
+  }, 
+  {
+    imgString: animals15,
+    price: '39.99'
+  }, 
+  {
+    imgString: animals16,
+    price: '34.99'
+  }, 
+  {
+    imgString: animals17,
+    price: '39.99'
+  }, 
+  {
+    imgString: animals18,
+    price: '34.99'
+  }, 
+  {
+    imgString: animals19,
+    price: '39.99'
+  }, 
+  {
+    imgString: animals20,
+    price: '34.99'
+  }, 
+  {
+    imgString: animals21,
+    price: '34.99'
+  }
+]
 
 
 
 export const Animals = () => {
+  const [singlePoster, setSinglePoster] = useState(false)
+  const [posterElementObject, setPosterElementObject] = useState({
+    price: '',
+    src: ''
+  })
+
+  const posterClickEvent = (event) => {
+  
+    const idString = event.target.id
+
+    const index = idString.split('-')[2]
+
+    setPosterElementObject({
+      price: arrayOfImages[index].price,
+      src: arrayOfImages[index].imgString
+    })
+  
+    // set page to render a single poster
+    setSinglePoster(true)
+
+    window.scrollTo(0, 0)
+  }
 
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
 
+  const arrayOfJSX = arrayOfImages.map((object, i) => {
+    return (
+      <div className="flex flex-col items-center justify-center" key={i+1}>
+        <img
+        src={object.imgString}
+        alt={`animals poster - ${i + 1}`}
+        className="border-black border-2 shadow-lg shadow-gray-800 transition ease-out hover:scale-105 duration-200 cursor-pointer"
+        id={`animals-poster-${i}`}
+        onClick={posterClickEvent}
+        />
+      </div>
+    )
+  })
+
+
+
   return (
     <div className="min-h-screen mb-24">
       
-      <div className="flex flex-col items-center justify-center h-[400px] bg-gray-600 border-b-2 border-black">
-        <h2 className="font-silkscreen font-extrabold text-6xl">ANIMALS</h2>
-      </div>
+      {
+      singlePoster ? (
 
-      <div className="grid grid-cols-3 gap-12 justify-items-center mx-8 my-16">
+        <div className="flex flex-row items-center justify-center mt-32">
+          <img
+          src={posterElementObject.src}
+          alt="single poster image"
+          className="w-[300px] mr-8 border-2 border-black shadow-lg shadow-gray-800"
+          />
 
-        {arrayOfJSX}
-        
-      </div>
+          <div className="bg-gray-600 py-8 px-8 border-2 border-black rounded-lg text-gray-300 ml-8 max-w-[400px]">
+            <p className="text-2xl mb-6">$ {posterElementObject.price}</p>
+            
+            <button className="bg-green-600 px-6 py-3 mx-auto rounded-lg hover:scale-105 duration-200 border-2 border-black text-xl w-full text-center mb-6 text-black">Add to Cart</button>
+          </div>
+        </div>
+
+      ) : (
+
+        <div>
+          <div className="flex flex-col items-center justify-center h-[400px] bg-gray-600 border-b-2 border-black">
+            <h2 className="font-silkscreen font-extrabold text-6xl">ANIMALS</h2>
+          </div>
+
+          <div className="grid grid-cols-3 gap-12 justify-items-center mx-8 my-16">
+            {arrayOfJSX}
+          </div>
+        </div>
+
+      )
+      }
 
     </div>
   )
